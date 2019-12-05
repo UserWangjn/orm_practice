b21eeb86e7e66f2a365f3a4daf2043c84754f946
wjn <wjntest@163.com>
2019-12-05 18:30:46 +0800

    Person表增加phone字段

--- app01/models.py
+++ app01/models.py
@@ -1,8 +1,25 @@
 from django.db import models
 
+class MyCharField(models.Field):
+    """
+    自定义的char类型的字段类
+    """
+
+    def __init__(self, max_length, *args, **kwargs):
+        self.max_length = max_length
+        super(MyCharField, self).__init__(max_length=max_length, *args, **kwargs)
+
+    def db_type(self, connection):
+        """
+        限定生成数据库表的字段类型为char，长度为max_length指定的值
+        """
+        return 'char(%s)' % self.max_length
+
+
 class Person(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=32)
     age = models.IntegerField()
     birth = models.DateTimeField(auto_now_add=True)
     modify_time = models.DateTimeField(auto_now=True)
+    phone = MyCharField(max_length=11)
